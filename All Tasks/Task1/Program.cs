c4b794fa9a82cc253aeebefcad97cd5a0b98df50
Александр Донченко <darkprince3738@gmail.com>
2021-06-08 14:03:04 +0300

    Task1

---
+++ All Tasks/Task1/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace Task1
+{
+    /*Task1​    
+    Перевизначте метод ToString базового типу object, який буде повертати гарно відформатовану інформацію. 
+    Створіть статичний клас Painter із методами void Paint(Car car, string colour), який міняє колір.
+    Виведіть інформацію про машину до і після пофарбування. Зробіть висновки і напішіть програму коректно.​*/
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car1 = new Car();
+            car1.Color = "white";
+            car1.Brand = "BMW";
+            car1.MaxSpeed = 250;
+            Painter.Paint(ref car1, "black");
+            Console.WriteLine($"{car1.Color}, {car1.Brand}, {car1.MaxSpeed}");
+        }
+    }
+    struct Car
+    {
+        private string color;
+        private string brand;
+        private int maxSpeed;
+
+        public string Color
+        {
+            set
+            {
+                if(value!="white" && value != "black" && value != "red")
+                {
+                    throw new Exception("Wrong color");
+                }
+                color = value;
+            }
+            get
+            {
+                return color;
+            }
+        }
+        public string Brand
+        {
+            set
+            {
+                if (value != "BMW" && value != "Mercedez" && value != "Mazda")
+                {
+                    throw new Exception("Wrong brand");
+                }
+                brand = value;
+            }
+            get
+            {
+                return brand;
+            }
+        }
+        public int MaxSpeed
+        {
+            set
+            {
+                if (value > 250)
+                {
+                    throw new Exception("Wrong max speed");
+                }
+                maxSpeed = value;
+            }
+            get
+            {
+                return maxSpeed;
+            }
+        }
+    }
+    static class Painter
+    {
+        public static void Paint(ref Car car1, string color)
+        {
+            car1.Color = color;
+        }
+    }
+}
---
+++ All Tasks/Task2/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Task2
+{
+    /*Використовуючи Visual Studio, створіть проект за шаблоном Console Application, назвіть його Lesson015_Task2.
+    Створіть клас PointClass і структуру PointStruct з автореалізованими властивостями int X, int У. 
+    Створіть статисний клас PerformanceTest із методами MeasureTestPointClass і MeasureTestPointStruct 
+    які прймають (int length), які створюють масив із довжиною length, і відповідно заповнюють його елементами.
+    Перевірте ефективність виконання цих методів, передаючи у параметр 1 000 000​*/
+    
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var stopWatch = new Stopwatch();
+
+            stopWatch.Start();
+            PermormanceTest.MeasureTestPointClass(100_000_000);
+            stopWatch.Stop();
+
+            Console.WriteLine($"PointClass {stopWatch.ElapsedMilliseconds}");
+            stopWatch.Reset();
+
+
+            stopWatch.Start();
+            PermormanceTest.MeasureTestPointStruct(100_000_000);
+            stopWatch.Stop();
+
+            Console.WriteLine($"PointStruct {stopWatch.ElapsedMilliseconds}");
+            stopWatch.Reset();
+        }
+    }
+    static class PermormanceTest
+    {
+        public static void MeasureTestPointClass(int length)
+        {
+            var random = new Random();
+
+            PointClass[] array = new PointClass[length];
+            for(int i = 0; i < length; i++)
+            {
+                array[i] = new PointClass(random.Next(-100,100), random.Next(-100, 100));
+            }
+        }
+        public static void MeasureTestPointStruct(int length)
+        {
+            var random = new Random();
+            PointStruct[] array = new PointStruct[length];
+            for (int i = 0; i < length; i++)
+            {
+                array[i] = new PointStruct(random.Next(-100, 100), random.Next(-100, 100));
+            }
+        }
+    }
+
+    class PointClass
+    {
+        public int X;
+        public int Y;
+
+        public PointClass(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+
+    struct PointStruct
+    {
+        public int X;
+        public int Y;
+
+        public PointStruct(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+    }
+}
